f4959508f6bf0710b384d972bf1d30d8fadbb2c7
Stas Ilyin <stasilyin.by@gmail.com>
2021-04-29 20:38:41 +0300

    add webpack.common

---
+++ configs/webpack.common.js
@@ -0,0 +1,70 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+const source = path.resolve(__dirname, '../src');
+const outputPath = path.resolve(__dirname, '../dist');
+const entryPath = path.resolve(__dirname, '../src/index.jsx');
+
+module.exports = {
+  entry: [entryPath],
+  output: {
+    path: outputPath,
+    filename: 'app[hash].js',
+    publicPath: '/',
+  },
+  resolve: {
+    extensions: [
+      '.js',
+      '.jsx',
+    ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        include: source,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.html$/,
+        use: 'html-loader',
+      },
+      {
+        test: /\.css$/,
+        use:
+          [
+            {
+              loader: 'style-loader',
+              options: {
+                esModule: true,
+                modules: {
+                  namedExport: false,
+                },
+              },
+            },
+            {
+              loader: 'css-loader',
+              options: {
+                esModule: true,
+                modules: {
+                  namedExport: false,
+                },
+              },
+            },
+          ],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      inject: 'body',
+      template: './src/index.html',
+    }),
+  ],
+};
