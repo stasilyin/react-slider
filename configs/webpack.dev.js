abf64757bde10da098e0900edfffa826d2d6156a
Stas Ilyin <stasilyin.by@gmail.com>
2021-05-01 14:03:47 +0300

    add dev config

---
+++ configs/webpack.dev.js
@@ -0,0 +1,31 @@
+const path = require('path')
+const { merge } = require('webpack-merge');
+
+const commonConfig = require('./webpack.common');
+
+const source = path.resolve(__dirname, '../src');
+
+module.exports = merge(commonConfig, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    port: 8080,
+    publicPath: '/',
+    hot: true,
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        include: source,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: require.resolve('babel-loader'),
+          },
+        ],
+      },
+    ],
+  },
+});
