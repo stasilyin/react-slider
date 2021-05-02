bd39005d9188abab270f0793b86f84da857954e5
Stas Ilyin <stasilyin.by@gmail.com>
2021-05-02 23:28:29 +0300

    added component structure

--- src/index.jsx
+++ src/index.jsx
@@ -1,1 +1,9 @@
-const hello = 'Hello World';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+	<React.StrictMode>
+	  
+	</React.StrictMode>,
+	document.getElementById('root'),
+);
