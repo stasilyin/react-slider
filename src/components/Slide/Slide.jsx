5663f51af053d7abeb56ca302630a29817810078
Stas Ilyin <stasilyin.by@gmail.com>
2021-05-03 13:17:29 +0300

    use babel-plugin-styled-components

--- src/components/Slide/Slide.jsx
+++ src/components/Slide/Slide.jsx
@@ -1,1 +1,34 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components/macro';
+
+const SlideWrapper = styled.div`
+  transition: transform 0.5s ease-out;
+  div {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    user-select: none;
+  }
+  img {
+    object-fit: cover;
+    object-position: center;
+  }
+`;
+function Slide({ child }) {
+  const slideRef = useRef('slide');
+
+  return (
+    <SlideWrapper ref={slideRef}>
+      <div>
+        { child }
+      </div>
+    </SlideWrapper>
+  );
+}
+
+Slide.PropTypes = {
+  child: PropTypes.element.isRequired,
+};
+
+export default Slide;
