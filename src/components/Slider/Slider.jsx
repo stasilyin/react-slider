bccc2c8eec6deaf5d13999c68a1e9d26b8e82e41
Stas Ilyin <stasilyin.by@gmail.com>
2021-05-03 14:04:55 +0300

    add styles for slider-wrapper

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,1 +1,39 @@
-import React from 'react';
+import React, {
+  useState,
+  useRef,
+  useLayoutEffect,
+  useEffect,
+  useCallback,
+} from 'react';
+import styled from 'styled-components/macro';
+import PropTypes from 'prop-types';
+import Slide from '../Slide/Slide';
+
+const SliderStyles = styled.div`
+  all: initial;
+  width: 100%;
+  height: 100%;
+  max-height: 100vh;
+  display: inline-flex;
+  will-change: transform, scale;
+  cursor: grab;
+  .slide-outer {
+    display: flex;
+    align-items: center;
+  }
+`;
+
+const SliderWrapper = styled.div`
+  overflow: hidden;
+  width: 100%;
+  height: 100%;
+  max-height: 100vh;
+`;
+
+function Slider({
+  children,
+}) {
+
+}
+
+export default Slider;
